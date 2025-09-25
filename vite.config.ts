7ead27daefc2e650d6dd311884264c0251e495ef
Nafiha Nalakath <nafinalakath666@gmail.com>
2025-09-25 12:39:20 -0700

    Deploy latest version

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
-  base: './',  // ✅ relative paths for custom domain
+  base: './', // ✅ relative paths for custom domain
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
